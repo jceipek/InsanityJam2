fa70bddcf6d63f2359685dbdebf88c9c27d7f358
Julian Ceipek <julian.ceipek@gmail.com>
2014-07-19 23:42:26 -0400

    Refactoring; bigger world

--- scripts/game.js
+++ scripts/game.js
@@ -5,12 +5,11 @@ define(['webglSupported', 'two', 'p2'], function (webglSupported, Two, P2) {
   var data = {
     world: null
   , entities: []
-  , two: null
   , CONSTS: {
       worldDims: {
-        width: 5
-      , height: 8
-      , zoom: 100
+        width: 30
+      , height: 50
+      , zoom: 1
       }
     }
   };
@@ -32,37 +31,45 @@ define(['webglSupported', 'two', 'p2'], function (webglSupported, Two, P2) {
   , init: function (elem) {
       var _g = this;
     
-      // Make an instance of two and place it on the page.
+      var renderer = _g.setupRenderer(elem);
+    
+      _g.resizeScene(renderer);
+      addEvent(window, 'resize', _g.resizeScene.bind(_g, renderer));
+      
+      _g.drawGameBorders(renderer);
+    
+      var world = _g.setupPhysics();
+      data.entities.push(_g.makeCircleEntity(world, renderer));
+    
+      var timeStep = 1 / 60;
+      setInterval(_g.physicsLoop.bind(_g, world, timeStep), 1000 * timeStep);
+      renderer.play();
+    }
+  , drawGameBorders: function (renderer) {
+      var DIMS = data.CONSTS.worldDims;
+      var rect = renderer.makeRectangle(0,0,DIMS.width,DIMS.height);
+      rect.stroke = "#eee";
+      rect.linewidth = 0.1;
+    }
+  , resizeScene: function (renderer) {
+      var DIMS = data.CONSTS.worldDims
+        , margin = 1
+        , scaleFactor = DIMS.zoom
+        , w = DIMS.width * scaleFactor + margin
+        , h = DIMS.height * scaleFactor + margin
+        , scale = Math.min(renderer.width / w, renderer.height / h) * scaleFactor;
+      renderer.scene.scale = scale;
+      renderer.scene.translation.x = renderer.width/2;
+      renderer.scene.translation.y = renderer.height/2;
+    }
+  , setupRenderer: function (domElement) {
       var params = { fullscreen: true };
       if (webglSupported) {
         params.type = Two.Types.webgl;
       } else {
         params.type = Two.Types.canvas;
       }
-      var two = new Two(params).appendTo(elem);
-      _g.resizeScene(two);
-      addEvent(window, 'resize', _g.resizeScene.bind(_g, two));
-    
-      var rect = two.makeRectangle(0,0,5,8);
-      rect.linewidth = 0.1;
-    
-      this.setupPhysics();
-      data.entities.push(_g.makeCircleEntity(data.world, two));
-    
-      var timeStep = 1 / 60;
-      setInterval(_g.physicsLoop.bind(_g, data.world, timeStep), 1000 * timeStep);
-      two.play();
-    }
-  , resizeScene: function (two) {
-      var DIMS = data.CONSTS.worldDims
-        , margin = 10
-        , scaleFactor = DIMS.zoom
-        , w = DIMS.width * scaleFactor + margin
-        , h = DIMS.height * scaleFactor + margin
-        , scale = Math.min(two.width / w, two.height / h) * scaleFactor;
-      two.scene.scale = scale;
-      two.scene.translation.x = two.width/2;
-      two.scene.translation.y = two.height/2;
+      return new Two(params).appendTo(domElement);
     }
   , setupPhysics: function () {
       var WORLD_HEIGHT = data.CONSTS.worldDims.height;
@@ -71,7 +78,6 @@ define(['webglSupported', 'two', 'p2'], function (webglSupported, Two, P2) {
       var world = new P2.World({
           gravity: [0, -9.82]
       });
-      data.world = world;
 
       // Create an infinite ground plane.
       var groundBody = new P2.Body({
@@ -81,6 +87,8 @@ define(['webglSupported', 'two', 'p2'], function (webglSupported, Two, P2) {
       var groundShape = new P2.Plane();
       groundBody.addShape(groundShape);
       world.addBody(groundBody);
+    
+      return world;
     }
   , physicsLoop: function (world, timeStep) {
       var i, entity
@@ -93,10 +101,10 @@ define(['webglSupported', 'two', 'p2'], function (webglSupported, Two, P2) {
         entity.graphic.translation.y = -entity.physicsBody.position[1];
       }
     }
-  , makeCircleEntity: function (world, two) {
+  , makeCircleEntity: function (world, renderer) {
       var radius = 1;
       var position = [0, 10];
-      var circleVis = two.makeCircle(position[0], position[1], radius);
+      var circleVis = renderer.makeCircle(position[0], position[1], radius);
       circleVis.fill = 'rgb(0, 0, 0)';
       circleVis.noStroke();
     
